bd2042341ebce16f760c733315a097d182b74e91
Marcelo Nobrega <marcelotmnobrega@gmail.com>
2019-04-20 13:13:03 -0300

    Adding logging message when creating a RestTemplate.

--- stock-service/src/main/java/com/mtmn/javamicro/stockservice/config/Config.java
+++ stock-service/src/main/java/com/mtmn/javamicro/stockservice/config/Config.java
@@ -1,6 +1,8 @@
 package com.mtmn.javamicro.stockservice.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,9 +10,11 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class Config {
 
-//    @LoadBalanced
+    //    @LoadBalanced
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate getRestTemplate() {
+        Logger logger = LoggerFactory.getLogger(Config.class);
+        logger.info("Creating a REST TEMPLATE");
         return new RestTemplate();
     }
 
